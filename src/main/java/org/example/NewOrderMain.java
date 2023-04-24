dda45cb10104aea473445d1868fcd3cb75004040
Ana Caroline Vilela <anacarolinevilela0@gmail.com>
2023-04-24 20:46:45 -0300

    extraindo a camada de producer

--- src/main/java/org/example/NewOrderMain.java
+++ src/main/java/org/example/NewOrderMain.java
@@ -1,39 +1,17 @@
 package org.example;
-
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
-        var key = UUID.randomUUID().toString();
-        var order = key + ", userId, orderValue";
-        var email = "Thanks for your order! We are processing it";
-        var orderRecord = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, order);
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
-        Callback callback = (data, ex) -> {
-            if(ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println("success sending " + data.topic() + ":::partition " + data.partition() + "/offset " + data.offset() + "/timestamp" + data.timestamp());
-        };
-        producer.send(orderRecord, callback).get();
-        producer.send(emailRecord, callback).get();
-    }
+        try (var dispatcher = new KafkaDispatcher()){
+            var key = UUID.randomUUID().toString();
+            var order = key + ", userId, orderValue";
+            var email = "Thanks for your order! We are processing it";
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
+            dispatcher.send("ECOMMERCE_NEW_ORDER", key, order);
+            dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+        }
+
     }
 }

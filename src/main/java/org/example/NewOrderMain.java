a283bf205653aafdbaab3ec49ec99ebe7c87a03b
Ana Caroline Vilela <anacarolinevilela0@gmail.com>
2023-04-24 21:47:26 -0300

    serialização com gson

--- src/main/java/org/example/NewOrderMain.java
+++ src/main/java/org/example/NewOrderMain.java
@@ -1,16 +1,22 @@
 package org.example;
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()){
-            var key = UUID.randomUUID().toString();
-            var order = key + ", userId, orderValue";
-            var email = "Thanks for your order! We are processing it";
+        try (var orderDispatcher = new KafkaDispatcher<Order>()){
+            try (var emailDispatcher = new KafkaDispatcher<String>()){
+                var userId = UUID.randomUUID().toString();
+                var orderId = UUID.randomUUID().toString();
+                var amount = new BigDecimal(Math.random() * 5000 + 1);
+                var order = new Order(userId, orderId, amount);
+                var email = "Thanks for your order! We are processing it";
 
-            dispatcher.send("ECOMMERCE_NEW_ORDER", key, order);
-            dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+                emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+            }
+
         }
 
     }

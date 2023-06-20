124cc3f6c5bacce9e995b0560cffb71e42657609
Ana Caroline Vilela <anacarolinevilela0@gmail.com>
2023-06-19 22:16:11 -0300

    aula: deserialização customizada

--- src/main/java/org/example/NewOrderMain.java
+++ src/main/java/org/example/NewOrderMain.java
@@ -6,12 +6,12 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         try (var orderDispatcher = new KafkaDispatcher<Order>()){
-            try (var emailDispatcher = new KafkaDispatcher<String>()){
+            try (var emailDispatcher = new KafkaDispatcher<Email>()){
                 var userId = UUID.randomUUID().toString();
                 var orderId = UUID.randomUUID().toString();
                 var amount = new BigDecimal(Math.random() * 5000 + 1);
                 var order = new Order(userId, orderId, amount);
-                var email = "Thanks for your order! We are processing it";
+                var email = new Email("Order received","Thanks for your order! We are processing it");
 
                 orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
                 emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
